b2cfb207948f038cbcbfaec375128e1df50319b4
sopeju toluwani <sopejutoluwani.t@gmail.com>
2025-11-29 20:07:39 +0100

    built investment account extension

--- Online Banking System/index.js
+++ Online Banking System/index.js
@@ -11,7 +11,11 @@ class Account {
   }
 
   deposit(amount) {
-    
+    if (amount <= 0) {
+      console.log("Deposited amount must be greater than 0");
+      return;
+    }
+
     if (amount >= 0) {
       this.#balance += amount;
       //console.log(`Deposited: $${amount}. New Balance: $${this.#balance}`);
@@ -24,7 +28,17 @@ class Account {
   }
 
   withdraw(amount) {
-    if (0 <= amount <= this.#balance) {
+    if (amount <= 0) {
+      console.log("Amount must be greater than 0");
+      return;
+    }
+
+    if (amount > this.#balance) {
+      console.log("Insufficient Funds");
+      return;
+    }
+
+    if (amount <= this.#balance) {
       this.#balance -= amount;
       //console.log(`Withdrew: $${amount}. New Balance: $${this.#balance}`);
       this.#recordtransaction(
@@ -63,10 +77,6 @@ class Account {
   }
 
   #recordtransaction(type, amount, description) {
-    this.type = type;
-    this.amount = amount;
-    this.description = description;
-
     this.#transactionsHistory.push({ type, amount, description });
     //console.log(this.#transactionsHistory)
   }
@@ -77,25 +87,190 @@ class Account {
   }
 
   //interest rate
-  #interestRate(){
-    return 20
+  #interestRate() {
+    return 0.25; //25% annual interest rate
   }
 
-  calculateInterest(time) {
-    const interest = (this.#balance * this.#interestRate() * time)/1200;
-    console.log(`Interest accured in ${time} months: $${interest}`);
+  calculateInterest(timeInMonths) {
+    const interest = (this.#balance * this.#interestRate() * timeInMonths) / 12;
+    console.log(`Interest accured in ${timeInMonths} months: $${interest}`);
+    return interest;
+  }
+}
+
+//Diffrent types of Account attached to Account class
+
+class SavingsAccount extends Account {
+  constructor(
+    accountNumber,
+    accountHolder,
+    initialBalance,
+    interestRate = 0.3,
+    minimumBalance = 100
+  ) {
+    super(accountNumber, accountHolder, initialBalance);
+    this.interestRate = interestRate;
+    this.minimumBalance = minimumBalance;
+  }
+
+  //Overriding withdraw method
+  withdraw(amount) {
+    //make sure minimum balance is maintained
+    if (this.getBalance() - amount < this.minimumBalance) {
+      // get balance from parent class then check if the withdraw will go below minimum balance
+      console.log(
+        `Cannot withdraw. Minimum balance of $${this.minimumBalance} must be maintained.`
+      );
+      return;
+    }
+    super.withdraw(amount); //call parent class withdraw method
+  }
+
+  //Overriding interest rate method
+  #interestRate() {
+    return this.interestRate;
+  }
+
+  calculateInterest(timeInMonths) {
+    const interest =
+      (this.getBalance() * this.interestRate * timeInMonths) / 12;
+    console.log(`Interest accured in ${timeInMonths} months: $${interest}`);
+    return interest;
+  }
+}
+
+//Checking Account
+class CheckingAccount extends Account {
+  constructor(
+    accountNumber,
+    accountHolder,
+    initialBalance,
+    overdraftLimit = 500
+  ) {
+    super(accountNumber, accountHolder, initialBalance);
+    this.overdraftLimit = overdraftLimit;
+  }
+
+  //Overriding withdraw method
+  withdraw(amount) {
+    if (amount > this.getBalance() + this.overdraftLimit) {
+      console.log("Withdrawal exceeds overdraft limit.");
+      return;
+    }
+    super.withdraw(amount); //call parent class withdraw method
+  }
+
+  //calculate interest method
+  calculateInterest(timeInMonths) {
+    console.log("Checking accounts do not accrue interest.");
+    return 0;
+  }
+
+  //Available overdraft
+  getAvailableBalanceToOverdraft() {
+    return this.getBalance() + this.overdraftLimit;
+  }
+}
+
+//investment account
+class InvestmentAccount extends Account {
+  constructor(
+    accountNumber,
+    accountHolder,
+    initialBalance,
+    riskLevel = "medium",
+    holdings = { stocks: [] }
+  ) {
+    super(accountNumber, accountHolder, initialBalance);
+    this.riskLevel = riskLevel;
+    this.holdings = holdings;
+  }
+
+  #interestRate() {
+    switch (this.riskLevel) {
+      case "low":
+        return 0.2; //20%
+      case "medium":
+        return 0.35; //35%
+      case "high":
+        return 0.5; //50%
+      default:
+        return 0.25; //default 25%
+    }
+  }
+
+  //implementing interest
+  calculateInterest(timeInMonths) {
+    const interest =
+      (this.getBalance() * this.#interestRate() * timeInMonths) / 12;
+    console.log(`Interest accured in ${timeInMonths} months: $${interest}`);
     return interest;
   }
 
+  //buystocks
+  buyStocks(stockName, shares, pricePerShare) {
+    const totalCost = shares * pricePerShare;
+    if (totalCost > this.getBalance()) {
+      //check if sufficient funds are available
+      console.log("Insufficient funds to buy stocks.");
+      return;
+    }
+    this.withdraw(totalCost);
+    this.holdings.stocks.push({
+      name: stockName,
+      shares: shares,
+      price: pricePerShare,
+    });
+    console.log(
+      `Bought ${shares} shares of ${stockName} at $${purchasePrice} each.`
+    );
+  }
 
+  //sellstocks
+  sellStocks(stockName, shares, pricePerShare) {
+    const index = this.holdings.stocks.findIndex(
+      (stock) => stock.name === stockName
+    );
 
+    if (index === -1) {
+      //checks if stock is owned.
+      console.log("you don't own this stock");
+      return;
+    }
+
+    //determine number of shares that can be sold
+    //how much shares is available
+    const currentShares = this.holdings.stocks[index];
+
+    if (shares > currentShares.shares) {
+      console.log(
+        `you don't have enough shares to sell. current shares: ${currentShares.shares}`
+      );
+      return;
+    }
+
+    totalCost = shares * pricePerShare;
+    this.deposit(totalCost);
+    // remove the sold shares from the owned shares
+    currentShares.shares -= shares;
+    console.log(
+      `Sold ${shares} shares of ${stockName}. Remaining: ${currentShares.shares}`
+    );
+
+    //remove stock from the holding if all shares are sold
+    if (currentShares === 0) {
+      console.log("all shares have been sold");
+      this.holdings.stocks.splice(index, 1);
+    }
+  }
+
+  //getting the total value of my portfolio
 }
-
 
 const acct1 = new Account(12345679809, "tolu");
 const acct2 = new Account(5555555555, "titus");
 acct1.deposit(1000);
-acct1.calculateInterest(10);
+acct1.calculateInterest(8);
 //acct1.withdraw(500);
 //acct1.transfer(acct2, 400);
 //acct1.getTransactionHistory();
